003c342293fc257390b21d1324ddd22a52f4f14e
Ivan Polovyi <ivan.polovyi@alianza.com>
2024-01-29 23:00:31 -0300

    "Final version"

--- spring-feign-client/src/main/java/com/polovyi/ivan/client/FeignClientConfig.java
+++ spring-feign-client/src/main/java/com/polovyi/ivan/client/FeignClientConfig.java
@@ -1,5 +1,19 @@
 package com.polovyi.ivan.client;
 
+import feign.Feign;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class FeignClientConfig {
+
+
+    @LoadBalanced
+    @Bean
+    public Feign.Builder feignBuilder() {
+        return Feign.builder();
+    }
+
 
 }
